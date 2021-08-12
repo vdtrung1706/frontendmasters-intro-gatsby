e703c13acdf44c31fd6bee412555b3b67ac8f712
Trung Vu <trunpyon.it@gmail.com>
2021-08-12 14:18:20 +0700

    using pages, Link

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { Link } from "gatsby";
+
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>About Page</h1>
+      <Link to="/">Back to Home</Link>
+    </main>
+  );
+}
