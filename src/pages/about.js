86398acb5e8d5b029a98dc48f65481145c423653
Trung Vu <trunpyon.it@gmail.com>
2021-08-13 11:59:29 +0700

    query image from graphql & in mdx

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,25 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
 import Layout from '../components/layout';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
-export default function AboutPage() {
+export const query = graphql`
+  query CocktailQuery {
+    file(name: { eq: "cocktail" }) {
+      childImageSharp {
+        gatsbyImageData(placeholder: DOMINANT_COLOR)
+      }
+    }
+  }
+`;
+
+export default function AboutPage({ data }) {
   return (
-    <Layout>
+    <Layout title="About this site">
+      <GatsbyImage
+        image={getImage(data.file)}
+        alt="a cocktail set inside an elaborate floral arrangement with dry ice mist curling out and around it"
+      />
       <h1>About Page</h1>
       <Link to="/">Back to Home</Link>
     </Layout>
